37add76a3aaeae163da974349699ea28e67c2301
graceewat <160866251+graceewat@users.noreply.github.com>
2024-05-06 16:00:32 -0400

    added apiURL

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ function submitSearch(event) {
   console.log(searchInput.value);
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
 
-let apiKey =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+function searchCity(city) {
+  let apiKey = "f03c7t04000f0dbod331aeefa47ad1f6";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiURL);
+}
