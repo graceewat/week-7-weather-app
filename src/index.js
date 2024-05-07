4e7cdcf0a578fd0b5695e5c2f6086a1458051be2
graceewat <160866251+graceewat@users.noreply.github.com>
2024-05-07 10:21:17 -0400

    Input weather and date data

--- src/index.js
+++ src/index.js
@@ -13,9 +13,44 @@ function searchCity(city) {
   axios.get(apiURL).then(refreshWeather);
 }
 function refreshWeather(response) {
+  //change temp
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
+  //change city
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = response.data.city;
+  //change conditions
+  let conditionsElement = document.querySelector("#conditions");
+  conditionsElement.innerHTML = response.data.condition.description;
+  //change humidity
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  //change wind
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = response.data.wind.speed;
+  //change time
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  console.log(date);
+  timeElement.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  console.log(minutes);
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
