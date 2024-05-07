642cdb0f5f290ca19a35493f34406107166e3f43
graceewat <160866251+graceewat@users.noreply.github.com>
2024-05-07 09:23:38 -0400

    Added API functionlaity to search

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 function submitSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  console.log(searchInput.value);
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
+searchCity("London");
 
 function searchCity(city) {
   let apiKey = "f03c7t04000f0dbod331aeefa47ad1f6";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiURL);
+  axios.get(apiURL).then(refreshWeather);
+}
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
 }
